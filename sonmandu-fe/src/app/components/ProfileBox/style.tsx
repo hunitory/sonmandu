45631b81c4a5510ab3a41f2c7698c2d78ea0a893
JinseongLee <kd0201e07@gmail.com>
2024-01-30 09:38:57 +0900

    chore: img, profilebox

---
+++ sonmandu-fe/src/app/components/ProfileBox/style.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { PALETTE } from "styles";
+
+export const ProfileBoxWrapper = styled.div`
+  display: flex;
+	width: fit-content;
+	gap: 12px;
+	align-items: center;
+	color: ${PALETTE.MAIN_BLACK};
+`

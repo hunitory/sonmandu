704f5d1736aa2781e0a424b70f629a51942e6235
Seungmin Lee <mehot98@gmail.com>
2024-02-06 00:38:34 +0900

    feat: add WebSocketSecurity
    Change-Id: I4342f0596cf58d52aa9823c61637c30ad5eca142

--- sonmandu-be/src/main/resources/static/app.js
+++ sonmandu-be/src/main/resources/static/app.js
@@ -1,18 +1,18 @@
+var token = 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiYXV0aCI6IlJPTEVfVVNFUiIsIm1lbWJlcklkIjoxLCJleHAiOjE3MDcxMjE1ODV9.rzOp-7C9Pdny1m7pkik_TBZ8uwaKmxZhk4QWSsr3J4XnUxY88UYQzdVMJG4_Rd46CTORcO2L160HYTz0FzO9fw'
+
 const stompClient = new StompJs.Client({
     brokerURL: 'ws://localhost:8080/chat-connection',
     connectHeaders: {
-    "Authorization" : 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiYXV0aCI6IlJPTEVfVVNFUiIsIm1lbWJlcklkIjoxLCJleHAiOjE3MDcxMjU5NDd9.0YXNPTGiD9BNj8xUt4o1ou4MAaiUV-0-DAi5FTkYKiu1Wmh42bdgjjmiG6SKg_Syu4-s-DUd37YuTVrrOxku4w'
+    "Authorization" : token
 }
 });
-
-var token = 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiYXV0aCI6IlJPTEVfVVNFUiIsIm1lbWJlcklkIjoxLCJleHAiOjE3MDcxMjE1ODV9.rzOp-7C9Pdny1m7pkik_TBZ8uwaKmxZhk4QWSsr3J4XnUxY88UYQzdVMJG4_Rd46CTORcO2L160HYTz0FzO9fw'
 
 stompClient.onConnect = (frame) => {
     setConnected(true);
     console.log('Connected: ' + frame);
     stompClient.subscribe('/topic/sonmandu', (chat) => {
         showGreeting(JSON.parse(chat.body).message);
-    }, {'Authorization': 'dasd'});
+    }, {'Authorization': token});
 };
 
 stompClient.onWebSocketError = (error) => {
@@ -48,7 +48,7 @@ function disconnect() {
 
 function sendName() {
     stompClient.publish({
-        // headers: {'Authorization': 'dasd'},
+        headers: {'Authorization': token},
         destination: "/app/sonmandu",
         body: JSON.stringify({'message': $("#name").val()})
     });

48fe99f61b450f01b20210b0a376489c77b2848a
이승민 <mehot98@gmail.com>
2024-02-07 13:13:26 +0900

    feat: change JwtFilter Exception Handle
    Change-Id: I8fe1773bbc907de828f9160eb15f13033c0cce59

--- sonmandu-be/src/main/java/com/nofriend/sonmandube/jwt/JwtFilter.java
+++ sonmandu-be/src/main/java/com/nofriend/sonmandube/jwt/JwtFilter.java
@@ -1,6 +1,7 @@
 package com.nofriend.sonmandube.jwt;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nofriend.sonmandube.exception.handler.ErrorMessage;
 =======
@@ -8,18 +9,30 @@ import com.nofriend.sonmandube.exception.RefreshTokenExpireException;
 import com.nofriend.sonmandube.exception.TokenDenyException;
 import com.nofriend.sonmandube.exception.TokenExpireException;
 >>>>>>> e2b9b34 (feat: change token exception)
+=======
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nofriend.sonmandube.exception.RefreshTokenExpireException;
+import com.nofriend.sonmandube.exception.TokenDenyException;
+import com.nofriend.sonmandube.exception.TokenExpireException;
+import com.nofriend.sonmandube.member.domain.Member;
+>>>>>>> d87b507 (feat: change JwtFilter Exception Handle)
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+<<<<<<< HEAD
+=======
+import org.springframework.http.HttpHeaders;
+>>>>>>> d87b507 (feat: change JwtFilter Exception Handle)
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.lang.model.type.ErrorType;
 import java.io.IOException;
 
 @Slf4j
@@ -48,31 +61,62 @@ public class JwtFilter extends OncePerRequestFilter {
         boolean hasToken = !accessToken.equals("null");
         boolean hasRefreshToken = !refreshToken.equals("null");
 
+
+
+
+
+    log.info(String.valueOf(jwtProvider.validateToken(accessToken)));
         if(!hasToken){}
         else if(jwtProvider.validateToken(accessToken) == JwtCode.ACCESS){
+<<<<<<< HEAD
 >>>>>>> c13f264 (feat: change token exception)
+=======
+
+>>>>>>> d87b507 (feat: change JwtFilter Exception Handle)
             Authentication authentication = jwtProvider.getAuthentication(accessToken);
+
             SecurityContextHolder.getContext().setAuthentication(authentication);
+
             request.setAttribute("memberId", authentication.getName());
+
         } else if (jwtProvider.validateToken(accessToken) == JwtCode.EXPIRED){
 
             if(!hasRefreshToken){
+                response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                objectMapper.writeValue(response.getWriter(), new Object());
                 throw new TokenExpireException("Expired access token");
             }
-            Authentication authentication = jwtProvider.getAuthentication(refreshToken);
-            String memberRefreshToken = jwtProvider.getRefreshToken(Long.valueOf(authentication.getName()));
 
-            if(jwtProvider.validateToken(refreshToken) == JwtCode.ACCESS && refreshToken.equals(memberRefreshToken)){
+
+
+            if(jwtProvider.validateToken(refreshToken) == JwtCode.ACCESS){
+                Authentication authentication = jwtProvider.getAuthentication(refreshToken);
+                String memberRefreshToken = jwtProvider.getRefreshToken(Long.valueOf(authentication.getName()));
+                if(refreshToken.equals(memberRefreshToken)){
                 String newAccessToken = jwtProvider.generateToken(authentication);
+
                 SecurityContextHolder.getContext().setAuthentication(authentication);
-                request.setAttribute("memberId", authentication.getName());
+
                 response.setHeader(HttpHeaders.AUTHORIZATION, newAccessToken);
-            } else if(jwtProvider.validateToken(refreshToken) == JwtCode.EXPIRED && refreshToken.equals(memberRefreshToken)){
+
+                }else {
+                    response.setStatus(HttpStatus.FORBIDDEN.value());
+                    objectMapper.writeValue(response.getWriter(), new Object());
+                    throw new TokenDenyException("Denied refresh token");
+                }
+
+            } else if(jwtProvider.validateToken(refreshToken) == JwtCode.EXPIRED){
+                response.setStatus(HttpStatus.GONE.value());
+                objectMapper.writeValue(response.getWriter(), new Object());
                 throw new RefreshTokenExpireException("Expired refresh token");
-            }else if(jwtProvider.validateToken(refreshToken) == JwtCode.DENIED && refreshToken.equals(memberRefreshToken)){
+            }else if(jwtProvider.validateToken(refreshToken) == JwtCode.DENIED){
+                response.setStatus(HttpStatus.FORBIDDEN.value());
+                objectMapper.writeValue(response.getWriter(), new Object());
                 throw new TokenDenyException("Denied refresh token");
             }
         } else if (jwtProvider.validateToken(accessToken) == JwtCode.DENIED){
+            response.setStatus(HttpStatus.FORBIDDEN.value());
+            objectMapper.writeValue(response.getWriter(), new Object());
             throw new TokenDenyException("Denied access token");
 >>>>>>> e2b9b34 (feat: change token exception)
         }
@@ -95,6 +139,13 @@ public class JwtFilter extends OncePerRequestFilter {
 
         String bearerToken = request.getHeader("Authorization");
 
+<<<<<<< HEAD
+=======
+    private String resolveToken(HttpServletRequest request, String header) {
+
+        String bearerToken = request.getHeader(header);
+
+>>>>>>> d87b507 (feat: change JwtFilter Exception Handle)
         if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")){
             return bearerToken.substring(7);
         }

8f6dc9b39d0a8d41b00da7b39550465a822f55e8
오지훈 <gfsd2397@naver.com>
2024-02-08 09:17:03 +0900

    chore: banner style 수정

--- sonmandu-fe/src/app/(pages)/(home)/@BannerSection/_style.tsx
+++ sonmandu-fe/src/app/(pages)/(home)/@BannerSection/_style.tsx
@@ -4,8 +4,8 @@ import { BaseButton } from 'components';
 import styled from 'styled-components';
 import { PALETTE, notoSansKr } from 'styles';
 
-
 export const MainBanner = styled.div`
+  position: relative;
   width: 100vw;
   height: 800px;
   display: flex;
@@ -13,17 +13,32 @@ export const MainBanner = styled.div`
   align-items: center;
   background-color: ${PALETTE.LIGHT_BLACK};
   opacity: 0.8;
+  img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    z-index: 1;
+  }
 `;
 
 export const MainTextContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   width: 50%;
   height: 320px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  z-index: 2;
 `;
 
 export const MainTitle = styled.div`
+  width: 725px;
   color: ${PALETTE.SUB_WHITE};
   font-size: 38px;
   font-family: ${notoSansKr.bold.style.fontFamily};
@@ -33,6 +48,7 @@ export const MainTitle = styled.div`
 
 export const Maincontent = styled.div`
   p {
+    width: 725px;
     color: ${PALETTE.SUB_WHITE};
     font-size: 18px;
     font-family: ${notoSansKr.medium.style.fontFamily};
@@ -42,7 +58,7 @@ export const Maincontent = styled.div`
 `;
 
 export const ButtonWrapper = styled.div`
-  width: 100%;
+  width: 725px;
   display: flex;
   justify-content: center;
   gap: 8%;

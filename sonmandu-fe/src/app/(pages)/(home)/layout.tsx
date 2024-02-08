cb1620812d3e28c56fbea900797e5f2858cd861e
오지훈 <gfsd2397@naver.com>
2024-02-08 09:06:26 +0900

    chore: main build 오류 수정

--- sonmandu-fe/src/app/(pages)/(home)/layout.tsx
+++ sonmandu-fe/src/app/(pages)/(home)/layout.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import * as Styled from './_style';
 
-export default function HomeLayout({ children }: { children: React.ReactNode }) {
-  return <Styled.StyledMain>{children}</Styled.StyledMain>;
+export default function HomeLayout({
+  children,
+  BannerSection,
+}: {
+  children: React.ReactNode;
+  BannerSection: React.ReactNode;
+}) {
+  return <Styled.StyledMain>{BannerSection}{children}</Styled.StyledMain>;
 }
--- sonmandu-fe/src/app/(pages)/(home)/page.tsx
+++ sonmandu-fe/src/app/(pages)/(home)/page.tsx
@@ -1,12 +1,12 @@
 'use client';
 
+import * as Styled from './_style';
 import BannerSection from './@BannerSection/page';
 
 export default function HomePage() {
 
   return (
     <>
-    <BannerSection />
     </>
   );
 }

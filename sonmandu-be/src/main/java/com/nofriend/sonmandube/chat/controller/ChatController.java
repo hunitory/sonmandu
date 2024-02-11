bab4b176cca69682742c576fc99cf9d0c0f82f48
Seungmin Lee <mehot98@gmail.com>
2024-02-11 16:57:48 +0900

    test: chatting principal
    Change-Id: Ifd0f3b6b90bddedf06fe10d51ad38c3bb5d36434

--- sonmandu-be/src/main/java/com/nofriend/sonmandube/chat/controller/ChatController.java
+++ sonmandu-be/src/main/java/com/nofriend/sonmandube/chat/controller/ChatController.java
@@ -16,7 +16,6 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
@@ -26,7 +25,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.security.Principal;
 import java.util.List;
-import java.util.Objects;
 
 @Controller
 @RequiredArgsConstructor
@@ -40,14 +38,14 @@ public class ChatController {
 
     @MessageMapping("/sonmandu")
     @SendTo("/topic/sonmandu")
-    public Chat chatting(@Valid ChatRequest chatRequest, SimpMessageHeaderAccessor accessor) {
+    public Chat chatting(Principal principal, @Valid ChatRequest chatRequest) {
     log.info("chatting1");
 
 
 //        token = token.substring(7);
 //
 //        Long memberId = Long.valueOf(jwtProvider.getAuthentication(token).getName());
-        Long memberId = Long.valueOf(Objects.requireNonNull(accessor.getUser()).getName());
+        Long memberId = Long.valueOf(principal.getName());
         log.info(String.valueOf(memberId));
         String memberNickname = memberRepository.findNicknameByMemberId(memberId).getNickname();
 

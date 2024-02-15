9184f4c78c96d9611331ffb4f8ef892e159af3ab
Seungmin Lee <mehot98@gmail.com>
2024-02-15 15:27:31 +0900

    fix: 500
    Change-Id: Ia755b5c0a340399e46aa7fb4518610813bd07fc2

--- sonmandu-be/src/main/java/com/nofriend/sonmandube/member/controller/request/LoginRequest.java
+++ sonmandu-be/src/main/java/com/nofriend/sonmandube/member/controller/request/LoginRequest.java
@@ -1,14 +1,9 @@
 package com.nofriend.sonmandube.member.controller.request;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.stereotype.Service;
-
-import java.awt.*;
 
 @Getter
 @Setter
